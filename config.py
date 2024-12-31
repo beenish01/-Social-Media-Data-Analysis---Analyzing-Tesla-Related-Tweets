ba4ef90ed7e950a9ff6b6c6a44bee7f4bf26f1c9
beenish01 <118468315+beenish01@users.noreply.github.com>
2025-01-01 00:18:29 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Kafka settings
+bootstrap_servers = 'localhost:9092'
+data_encoding = 'utf-8'
+twitter_kafka_topic_name = 'twitter2'
+
+# Database settings
+tweets_database_path = 'tweets.db'
+tweets_table_name = 'tesla'
